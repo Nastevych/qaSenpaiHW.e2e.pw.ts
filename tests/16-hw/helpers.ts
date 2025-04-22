f1717d90b6e301e2498ce3a516d24d34f2e7b02e
Nastevych <n.ilya93@gmail.com>
2025-04-22 18:52:58 +0300

    Refactor deleteAuthDataFileForUser function to accept userRole instead of userEmail for improved clarity and consistency.

--- tests/16-hw/helpers.ts
+++ tests/16-hw/helpers.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 import path from "path";
 
-export function deleteAuthDataFileForUser (userEmail: string) {
-fs.readdirSync("tests/16-hw/.auth").forEach((file) => {
-    if (file.includes(`${userEmail}`)) {
+export function deleteAuthDataFileForUser(userRole: string) {
+  fs.readdirSync("tests/16-hw/.auth").forEach((file) => {
+    if (file.includes(`${userRole}`)) {
       fs.rmSync(path.join("tests/16-hw/.auth", file));
     }
   });
